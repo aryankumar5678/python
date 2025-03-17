475a8602083b4c9d5561310a66bcdce8a1ee1a81
aryan kumar <114462963+aryankumar5678@users.noreply.github.com>
2025-03-17 16:44:30 +0530

    Add files via upload

---
+++ program 3.py
@@ -0,0 +1,9 @@
+n1 = int(input("Enter the first number"))
+n2 = int(input("Enter the second number"))
+n3 = int(input("Enter the third number"))
+if(n1> n2 and n1>n3):
+    print(n1)
+elif(n2>n1 and n2>n3):
+    print(n2)
+elif(n3>n1 and n3>n2):
+    print(n3)
