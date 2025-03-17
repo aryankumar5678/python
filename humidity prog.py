1a8285b1d8230c93e6f7899fa844b60baefab699
aryan kumar <114462963+aryankumar5678@users.noreply.github.com>
2025-03-17 16:42:54 +0530

    Add files via upload

---
+++ humidity prog.py
@@ -0,0 +1,6 @@
+n1 = int(input("Enter the temperature in degree celsius"))
+n2 = int(input("Enter the humidity levels"))
+if(n1 >20 and n2>50):
+    print("Weather is suitable for going outside")
+else:
+    print("Invalid")
