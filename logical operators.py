2ccfbef7fe0a42721754e0c8cf147cfec4167108
aryan kumar <114462963+aryankumar5678@users.noreply.github.com>
2025-03-17 16:43:19 +0530

    Add files via upload

---
+++ logical operators.py
@@ -0,0 +1,7 @@
+n1 = int(input("Enter the first number"))
+n2 = int(input("Enter the second number"))
+n3 = int(input("Enter the third number"))
+if(n1>10 and n2>10 and n3>10):
+    print(n1,n2,n3)
+else:
+    print("Invalid")
